d86633c75bcda5914f273511ea6f7a1acc23227f
juan-ramon-lastra <juanra11.lastra@gmail.com>
2022-03-04 13:25:42 +0100

    añadida seguridad jwt

--- apirest-productos/src/main/java/com/formacionspring/apirest/ApirestProductosApplication.java
+++ apirest-productos/src/main/java/com/formacionspring/apirest/ApirestProductosApplication.java
@@ -1,13 +1,28 @@
 package com.formacionspring.apirest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class ApirestProductosApplication {
+public class ApirestProductosApplication implements CommandLineRunner {
 
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ApirestProductosApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		String pass = "hola1";
+		for (int i = 0; i < 3; i++) {
+			String passBCrypt = passwordEncoder.encode(pass);
+			System.out.println(passBCrypt);
+		}
 	}
 
 }
--- apirest-productos/src/main/resources/import.sql
+++ apirest-productos/src/main/resources/import.sql
@@ -3,3 +3,14 @@ INSERT INTO productos(codigo, tipo, cantidad, precio, marca, fecha_ingreso, desc
 INSERT INTO productos(codigo, tipo, cantidad, precio, marca, fecha_ingreso, descripcion) VALUES ('eeekkk','juguete',6,45.95,'Barbie','2010-11-05','Muñecas de barbie');
 INSERT INTO productos(codigo, tipo, cantidad, precio, marca, fecha_ingreso, descripcion) VALUES ('llowwl','comida',2,13.75,'Gallo','2021-03-09','Diferentes platos de pasta');
 INSERT INTO productos(codigo, tipo, cantidad, precio, marca, fecha_ingreso, descripcion) VALUES ('jajaja','vehiculo',5,4600,'Suzuki','2010-10-02','Motocicleta de segunda mano');
+
+INSERT INTO usuarios (username, password, enabled) VALUES ('juan', '$2a$10$WVh4dBD7WxPXBd82ZCxHNOmd/wemrdxfzvK7.Rwzbqh0QaoUrQwdC', 1);
+INSERT INTO usuarios (username, password, enabled) VALUES ('admin', '$2a$10$qXUacjhDtl4Gh.PdC28c6.vljAjg./kjrWb.v3xuG5VtOJHxmguGe', 1);
+INSERT INTO usuarios (username, password, enabled) VALUES ('pepe', '$2a$10$Wkoc/2cuB1nQzC3BKHHZUesuKkP/B9G.5yT5ZVDP1HreevGjTibbq', 1);
+
+INSERT INTO roles (nombre) VALUES ('ROLE_USER');
+INSERT INTO roles (nombre) VALUES ('ROLE_ADMIN');
+
+INSERT INTO usuarios_roles ('usuario_id', 'role_id') VALUES (1, 1);
+INSERT INTO usuarios_roles ('usuario_id', 'role_id') VALUES (2, 2);
+INSERT INTO usuarios_roles ('usuario_id', 'role_id') VALUES (2, 1);
